41a73ed0dc8b0685d0a07a59bc3fc27dbad40f7e
Tiro Mosoeu <tmosoeu@c4r7s10.wethinkcode.co.za>
2020-06-26 16:21:10 +0200

    ex00 is done

---
+++ ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmosoeu <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/26 16:17:57 by tmosoeu           #+#    #+#             */
+/*   Updated: 2020/06/26 16:20:45 by tmosoeu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<stdio.h>
+#include<unistd.h>
+
+void ft_swap(int a*, int *b);
+int ft_strcmp(char *s1, char *s2);
+int ft_strlen(char *str);
+void ft_putchar(char c);
+void ft_putstr(char *str);
+
